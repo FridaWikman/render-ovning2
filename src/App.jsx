5ed57dfd0aca42dee4bba469f40a0fb779ab4165
FridaWikman <frida.wikman@hotmail.com>
2024-10-22 11:52:35 +0200

    deploy hook first try

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React and my changes</h1>
+      <h1>Vite + React and my new changes</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
